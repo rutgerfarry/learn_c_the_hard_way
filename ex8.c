a26d3a4215180a1e7733bb2a9c4f5657745b9bed
Rutger Farry <rutgerfarry@gmail.com>
2016-08-20 19:08:48 -0700

    ex8 with book v2

--- ex8.c
+++ ex8.c
@@ -2,33 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-	int areas[] = {10, 12, 13, 14, 20};
-	char name[] = "Rutger";
-	char full_name[] = {
-		'R', 'u', 't', 'g', 'e', 'r',
-		' ', 'V', '.', ' ',
-		'F', 'a', 'r', 'r', 'y', '\0'
-	};
+	int i = 0;
 
-	printf("The size of an int: %ld\n", sizeof(int));
-	printf("The size of areas (int[]): %ld\n",
-		sizeof(areas));
-	printf("The number of ints in areas: %ld\n",
-		sizeof(areas) / sizeof(int));
-	printf("The first area is %d, the 2nd is %d.\n",
-		areas[0], areas[1]);
+	if (argc == 1) {
+		printf("You only have one argument. You suck.\n");
+	} else if (argc > 1 && argc < 4) {
+		printf("Here's your arguments:\n");
 
-	printf("The size of a char: %ld\n", sizeof(char));
-	printf("The size of name (char[]): %ld\n",
-		sizeof(name));
-	printf("The number of chars: %ld\n",
-		sizeof(name) / sizeof(char));
+		for (i = 0; i < argc; i++) {
+			printf("%s ", argv[i]);
+		}
+		printf("\n");
+	} else {
+		printf("You have too many arguments. You suck.\n");
+	}
 
-	printf("The size of full_name (char[]): %ld\n",
-		sizeof(full_name));
-	printf("The number of chars: %ld\n",
-		sizeof(full_name) / sizeof(char));
-
-	printf("name=\"%s\" and full_name=\"%s\"\n",
-		name, full_name);
+	return 0;
 }
